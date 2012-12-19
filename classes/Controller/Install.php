9004a340146d903afaa75dafd39d4f37873f67f3
Justin Shanks <jshanman@gmail.com>
2012-12-19 00:50:47 -0600

    added install file

---
+++ classes/Controller/Install.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+  * this file will read all model and field classes from application and core (/modules/supermodlr/classes) and pull them into the configured db if they don't exist yet
+  *
+  */
+class Supermodlr_Install extends Controller_Page {
+	public function action_index() 
+	{
+		//open all core models
+
+			//read the file
+
+			//store it in the local db
+
+		//open all core fields
+
+			//read the file
+
+			//store it in the local db
+	}
+
+
+}
