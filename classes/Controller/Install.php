f280e5adb7d87aa54e0f567d55ba2946349ae531
Justin Shanks <jshanman@gmail.com>
2012-12-19 14:56:04 -0600

    started writing install/import logic

--- classes/Controller/Install.php
+++ classes/Controller/Install.php
@@ -4,21 +4,84 @@
   * this file will read all model and field classes from application and core (/modules/supermodlr/classes) and pull them into the configured db if they don't exist yet
   *
   */
-class Supermodlr_Install extends Controller_Page {
+class Controller_Install extends Controller {
+
 	public function action_index() 
 	{
-		//open all core models
+		//@todo support looping through all other modules as well
+		$paths = array(
+			APPPATH.'classes'.DIRECTORY_SEPARATOR,
+			MODPATH.'supermodlr'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR,
+		);
 
-			//read the file
+		foreach ($paths as $path) 
+		{
+			$model_path = $path.'Model'.DIRECTORY_SEPARATOR;
 
-			//store it in the local db
+			if (!is_dir($model_path)) continue;
+			
+			//open all models
+			$models = scandir($model_path);
+			foreach ($models as $filename) 
+			{
+				if (substr($filename,0,1) == '.') continue;
 
-		//open all core fields
+				//get file info
+				$info = pathinfo($model_path.$filename);
+				$class_name = 'Model_'.$info['filename'];
+echo $class_name.'<br/>';
+				//if this file hasn't already been included (and do not allow autoload)
+				if (!class_exists($class_name,FALSE)) 
+				{
+					//read the file
+					include $model_path.$filename;
 
-			//read the file
+					$refclass = new ReflectionClass($class_name);
 
-			//store it in the local db
+					$extends = get_parent_class($class_name);
+
+					echo $class_name.' extends '.$extends.'<br/>';
+
+					foreach ($refclass->getProperties() as $property) 
+					{
+						echo $property.'<br/>';
+					}
+
+					//store it in the local db					
+				}
+
+			}
+			
+
+
+
+			//open all fields
+
+				//read the file
+
+				//store it in the local db
+
+		}
+
+ob_end_flush();
+
 	}
 
+
+	public function class_to_json_object($class_name) 
+	{
+		$json = array();
+		$Class = new ReflectionClass($class_name);
+
+		foreach ($refclass->getProperties() as $property) 
+		{
+
+		}
+
+
+	}
+
+
+	//@todo use reflection export to get the source code for each method
 
 }
