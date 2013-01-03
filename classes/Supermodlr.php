3dc570198bd85f952ec0273881b1356f3f95dfe5
Justin Shanks <jshanman@gmail.com>
2013-01-03 16:12:01 -0600

    moved default config to config file

---
+++ classes/Supermodlr.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Supermodlr extends Supermodlr_Core {
+	public static $__scfg = array(
+		'framework_name' => 'Kohana',
+	);
+}
---
+++ config/supermodlr.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	'drivers_config' => array(
+			array(
+					'name'     => 'coremongo',
+					'driver'   => 'Supermodlr_Mongodb',
+					'host'     => '127.0.0.1',
+					'port'     => '27017',
+					'user'     => '',
+					'pass'     => '',
+					'dbname'   => 'supermodlr',
+					'replset'  => FALSE,
+					'safe'     => TRUE,
+					'fsync'    => FALSE,
+			)
+	),		
+);
