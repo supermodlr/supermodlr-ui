c3ec080c0e632f19aa8d3d29125f0f63d19ca1b5
jshanks <jshanks@cygnus.com>
2013-01-17 10:06:29 -0600

    set db config to primary

--- config/supermodlr.php
+++ config/supermodlr.php
@@ -1,19 +1,19 @@
 <?php
 $driver_config = array(
-	array(
-			'name'     => 'coremongo',
-			'driver'   => 'Supermodlr_Mongodb',
-			'host'     => '127.0.0.1',
-			'port'     => '27017',
-			'user'     => '',
-			'pass'     => '',
-			'dbname'   => 'supermodlr',
-			'replset'  => FALSE,
-			'safe'     => TRUE,
-			'fsync'    => FALSE,
-	)
+    'primary'=> array(
+            'name'     => 'coremongo',
+            'driver'   => 'Supermodlr_Mongodb',
+            'host'     => '127.0.0.1',
+            'port'     => '27017',
+            'user'     => '',
+            'pass'     => '',
+            'dbname'   => 'supermodlr',
+            'replset'  => FALSE,
+            'safe'     => TRUE,
+            'fsync'    => FALSE,
+    )
 );
 return array(
-	'field.drivers_config' => $driver_config,
-	'model.drivers_config' => $driver_config,	
+    'field.drivers_config' => $driver_config,
+    'model.drivers_config' => $driver_config,   
 );
